7e68c1b5a185930115b2cf4e16346fe251d6be05
lvigerust <l.vigerust@gmail.com>
2020-11-01 14:27:26 +0100

    final fix

--- script.js
+++
@@ -1,9 +0,0 @@
-var click = document.getElementsByClassName("click")[0];
-
-click.addEventListener("touchstart", function(){
-    click.classList.add("flip-card-back");
-})
-
-click.addEventListener("touchend", function(){
-    click.classList.remove("flip-card-back");
-})
